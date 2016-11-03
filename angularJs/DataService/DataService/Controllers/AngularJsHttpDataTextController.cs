b6286e61592338f73ee97e3696c4177c8bf78bd6
aaron <terminator_aaron@hotmail.com>
2016-11-03 16:05:41 +0800

    angularjs

---
+++ angularJs/DataService/DataService/Controllers/AngularJsHttpDataTextController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace DataService.Controllers
+{
+    public class AngularJsHttpDataTextController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            var stream = new FileStream(@"D:\MyLearn\AngularJs\runoob.com\DataService\DataService\JsonData\Person.json", FileMode.Open);
+
+            var result = Request.CreateResponse(HttpStatusCode.OK);
+            result.Content = new StreamContent(stream);
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            return result;
+        }
+
+        [Route("ValidSuccess")]
+        public bool ValidSuccess(string username)
+        {
+            return username=="aaron";
+        }
+    }
+}
